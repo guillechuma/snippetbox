a8ab7d32b507bae2560571638cdeef368d994c4b
Guillermo Chumaceiro <guillechuma24@gmail.com>
2025-04-08 00:34:49 +0200

    Added static serve dir and style to the html templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	// Initialize servemux (router)
 	mux := http.NewServeMux()
+
+	// Create file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
