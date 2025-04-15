b005a2cf9912a043dbeac53e95017f85a792b5bb
Guillermo Chumaceiro <guillechuma24@gmail.com>
2025-04-15 23:26:57 +0200

    Cleaner code structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,23 +20,12 @@ func main() {
 
 	app := &application{logger: logger}
 
-	// Initialize servemux (router)
-	mux := http.NewServeMux()
-
-	// Create file server
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	// Log statements
 	logger.Info("starting server", "addr", *addr)
 
 	// Start a new web server
 	// Parameters: TCP network adress to listen,and servemux
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
